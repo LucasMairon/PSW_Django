b533a7490e6aeb2be587e253903e04ba8b5d3b7a
LucasMairon <lucasmairon2@gmail.com>
2024-01-20 17:11:31 -0300

    adicionando funcionalidades de apostilas

--- apostilas/admin.py
+++ apostilas/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Apostila
+from .models import Apostila, ViewsApostila
 
 admin.site.register(Apostila)
+admin.site.register(ViewsApostila)
--- apostilas/views.py
+++ apostilas/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Apostila
+from .models import Apostila, ViewsApostila
 from django.contrib.messages import constants
 from django.contrib import messages
 
 def adicionar_apostilas(request):
     if request.method == "GET":
         apostilas = Apostila.objects.filter(user = request.user)
-        return render(request, 'adicionar_apostilas.html',{'apostilas' : apostilas})
+    # TODO criar as tags
+        views_totais = ViewsApostila.objects.filter(apostila__user = request.user).count()
+        return render(request, 'adicionar_apostilas.html',{'apostilas' : apostilas, 'views_totais' : views_totais})
     elif request.method == "POST":
         titulo = request.POST.get('titulo')
-        arquivo = request.FILES('arquivo')
+        arquivo = request.FILES['arquivo']
 
         apostila =  Apostila(
             user = request.user,
@@ -20,3 +22,15 @@ def adicionar_apostilas(request):
         apostila.save()
         messages.add_message(request, constants.SUCCESS, 'Salvo com sucesso')
         return redirect('/apostilas/adicionar_apostilas')
+    
+def apostila(request, id):
+    apostila = Apostila.objects.get(id = id)
+
+    views_totais = ViewsApostila.objects.filter(apostila = apostila).count()
+    views_unicas = ViewsApostila.objects.filter(apostila = apostila).values('ip').distinct().count()
+    view = ViewsApostila(
+        ip = request.META['REMOTE_ADDR'],
+        apostila = apostila
+    )
+    view.save()
+    return render(request, 'apostila.html', {'apostila' : apostila, 'views_totais' : views_totais, 'views_unicas' : views_unicas})
